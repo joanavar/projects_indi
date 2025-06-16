15c78ffeb13b9e3eca617777c1c6252ae75a7baf
joannavarrogomez <joannavarrogomez@gmail.com>
2025-06-16 18:18:02 +0200

    Mi primer bd

--- lista_tareas/appy.py
+++ lista_tareas/appy.py
@@ -1,24 +1,33 @@
-from flask import Flask, request, session, render_template, redirect, url_for
+from flask import Flask, redirect, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key= "Clave_secreta_lista"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tareas.db" # Archivo DB local
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Tarea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(200), nullable = False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if "tareas" not in session:
-        session["tareas"] = []
     if request.method == "POST":
         accion = request.form.get("accion")
         if accion == "agregar":
             tarea_nueva = request.form.get("tarea")
             if tarea_nueva:
-                session["tareas"].append(tarea_nueva)
-                session.modified = True
+                nueva_tarea = Tarea(descripcion= tarea_nueva)
+                db.session.add(nueva_tarea)
+                db.session.commit()
         elif accion == "Borrar_todo":
-            session["tareas"] = []
-            session.modified = True
+            Tarea.query.delete()
+            db.session.commit()
         return redirect(url_for("index"))
-    return render_template("index.html", tareas=session["tareas"])
+    tareas = Tarea.query.all()
+    return render_template("index.html", tareas=tareas)
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-    
+    app.run(debug=True, port=5002)
